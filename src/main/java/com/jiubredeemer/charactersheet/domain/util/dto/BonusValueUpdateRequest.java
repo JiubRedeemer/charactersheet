80e0c05d80fa12e853c9a1110bbe47780e086eb0
jiubredeemer <jiubredeemer@gmail.com>
2025-04-15 19:59:00 +0400

    PRO-0: fix bonus value dto

--- src/main/java/com/jiubredeemer/charactersheet/domain/util/dto/BonusValueUpdateRequest.java
+++ src/main/java/com/jiubredeemer/charactersheet/domain/util/dto/BonusValueUpdateRequest.java
@@ -2,7 +2,13 @@ package com.jiubredeemer.charactersheet.domain.util.dto;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class BonusValueUpdateRequest {
     private Long bonusValue;
+
+    public void setBonusValue(Long bonusValue) {
+        this.bonusValue = Objects.requireNonNullElse(bonusValue, 0L);
+    }
 }
